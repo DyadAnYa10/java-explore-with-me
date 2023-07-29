100ac80d65f3b44b32618221b088076584ee9d04
DyadAnYa10 <anna.dyadyushkina@yandex.ru>
2023-07-29 22:24:02 +0300

    fix bugs

--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsRepository.java
@@ -11,23 +11,23 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<ItemStats, Long> {
     @Query("select new ru.practicum.statsdto.StatDto(h.app, h.uri, count(h.ip)) " +
             "from ItemStats h " +
-            "where h.timestamp between ?1 and ?2 " +
+            "where h.timestamp between :start and :end " +
             "group by h.app, h.uri " +
             "order by count(h.ip) desc")
     List<StatDto> findAllWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.statsdto.StatDto(h.app, h.uri, count(h.ip)) " +
             "from ItemStats h " +
-            "where h.timestamp between ?1 and ?2 " +
-            "and h.uri in (?3) " +
+            "where h.timestamp between :start and :end " +
+            "and h.uri in (:uris) " +
             "group by h.app, h.uri " +
             "order by count(h.ip) desc")
     List<StatDto> findAllNotUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.statsdto.StatDto(h.app, h.uri, count(distinct h.ip)) " +
             "from ItemStats h " +
-            "where h.timestamp between ?1 and ?2 " +
-            "and h.uri in (?3) " +
+            "where h.timestamp between :start and :end " +
+            "and h.uri in (:uris) " +
             "group by h.app, h.uri " +
             "order by count(distinct h.ip) desc")
     List<StatDto> findAllUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
